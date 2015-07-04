e3c8276ea52cfebce17cb4db0cec287e333a7b8a
James Halliday <mail@substack.net>
2015-07-04 03:49:15 -0700

    two passing, one failing test

---
+++ test/pack.js
@@ -0,0 +1,35 @@
+var test = require('tape');
+var packer = require('../');
+
+test('read 2 of a single byte', function (t) {
+    var p = packer();
+    p.write(Buffer([ parseInt('10101', 2) ]));
+    var out = p.read(2);
+    t.equal(bin(out), '0' + '00010101' + '1' + '111111');
+    t.end();
+});
+
+test('read 3 of a single byte', function (t) {
+    var p = packer();
+    p.write(Buffer([ parseInt('10101', 2) ]));
+    var out = p.read(3);
+    t.equal(bin(out), '0' + '00010101' + '1' + '111111' + '11111111');
+    t.end();
+});
+
+test('read 3 of two bytes', function (t) {
+    var p = packer();
+    p.write(Buffer([ parseInt('11011011', 2), parseInt('01110010', 2) ]));
+    var out = p.read(3);
+    t.equal(bin(out), '0' + '11011011' + '10' + '01110010' + '11111');
+    t.end();
+});
+
+function zpad (n, s) { return Array(n+1-s.length).join('0') + s }
+function bin (buf) {
+    var s = '';
+    for (var i = 0; i < buf.length; i++) {
+        s += zpad(8, buf[i].toString(2));
+    }
+    return s;
+}
